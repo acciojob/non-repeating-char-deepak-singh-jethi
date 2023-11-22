1a066006f4da2af7f7e8bb09244677e4f26399a7
DEEPAK SINGH JETHI <134581381+deepak-singh-jethi@users.noreply.github.com>
2023-11-22 18:33:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+  for (let i = 0; i < str.length; i++) {
+    let count = 0;
+    let ch = str.charAt(i);
+    for (let j = 0; j < str.length; j++) {
+      if (j != i && ch == str.charAt(j)) {
+        count++;
+        break;
+      }
+    }
+
+    if (count == 0) {
+      return ch;
+    }
+  }
+  return null;
 }
 const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+alert(firstNonRepeatedChar(input));
